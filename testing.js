f56e9a3d913378a13493fcbf9ba1ef3d671901f4
salvator86 <demkov.sasha42@gmail.com>
2021-09-20 17:16:37 +0000

    added code

--- testing.js
+++ testing.js
@@ -1,1 +1,28 @@
+// alert('Hello!');
 
+// const message = 'Hello!';
+// aler(message);
+
+// let counter = 0;
+// counter = counter + 1;
+// counter = counter + 1;
+// counter += 1;
+// counter++;
+// alert(counter);
+
+const userAge = parseFloat(prompt('Enter your age'));
+alert('Your entered ' + userAge);
+
+const nextAge = add(userAge, 1);
+alert('Your next age ' + nextAge);
+
+if (userAge >= 18) { 
+    alert('You are adult');
+} else {
+    alert('You are underaged');
+}
+
+function add(a, b) {
+    const result = a + b;
+    return result;
+}
