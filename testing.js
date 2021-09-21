f454c1ea687a9797631529eab2616a932f408f72
salvator86 <demkov.sasha42@gmail.com>
2021-09-21 11:15:41 +0000

    added two first tasks

--- testing.js
+++ testing.js
@@ -1,28 +1,26 @@
-// alert('Hello!');
+//Task-1-1
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals; // code goes here
+  }
 
-// const message = 'Hello!';
-// aler(message);
+//Sample Tests:
+describe("Tests", () => {
+    it("test", () => {
+  Test.assertEquals(goals(0,0,0), 0)
+  Test.assertEquals(goals(43, 10, 5), 58)
+    });
+  });
 
-// let counter = 0;
-// counter = counter + 1;
-// counter = counter + 1;
-// counter += 1;
-// counter++;
-// alert(counter);
-
-const userAge = parseFloat(prompt('Enter your age'));
-alert('Your entered ' + userAge);
-
-const nextAge = add(userAge, 1);
-alert('Your next age ' + nextAge);
-
-if (userAge >= 18) { 
-    alert('You are adult');
-} else {
-    alert('You are underaged');
-}
-
-function add(a, b) {
-    const result = a + b;
-    return result;
-}
+//Task-1-2
+function move (position, roll) {
+    return position + roll*2;// return the new position
+  }
+//Sample Tests:
+describe('move function', () => {
+    it('basic tests', () => {
+      Test.assertEquals(move(0, 4), 8);
+      Test.assertEquals(move(3, 6), 15);
+      Test.assertEquals(move(2, 5), 12);
+    })
+  })
+//Task-1-3
